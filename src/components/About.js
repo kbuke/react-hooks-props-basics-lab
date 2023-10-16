2d8783237ae1c7ab5f3c4afeb2b1f57cfe653d5c
kaan <kabuke13@gmail.com>
2023-10-16 18:52:38 +0100

    lab changes

--- src/components/About.js
+++ src/components/About.js
@@ -1,14 +1,29 @@
 import React from "react";
+import Links from "./Links"
 
-function About() {
+// Update the About component so that:
+
+// If a prop of bio is passed to this component, it is displayed inside of a <p> tag
+// If a prop of bio is not passed down, or if bio is an empty string, don't display the <p> tag
+// Hint: you'll need to do some conditional rendering to an external site. here to determine whether or not to display the <p> tag
+
+
+
+function About(props) {
+  if(!props.bio || props.bio===""){
+    return null
+  } else {
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      <p>{props.bio}</p>
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
+      {/* add your <Links /> component here */
+      <Links github={props.links.github} linkedin={props.links.linkedin}/>
+      }
     </div>
   );
+  }
 }
 
 export default About;
--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,16 @@ import Home from "./Home";
 import About from "./About";
 
 // pass this data down as props to the child component(s) that need it!
+// In App component, pass down data from user object as props to Home component so it renders correctly. 
+//There is some starter code for you in Home component that should give you clues as to what props this component takes in.
+
+// App should also pass down a prop of bio to the About component.
+
+// Finally, App should pass down github and linkedin links to the About component, 
+//so that About can pass those props down to the Links component you will create.
+
+
+
 import user from "../data/user";
 console.log(user);
 
@@ -11,8 +21,8 @@ function App() {
   return (
     <div>
       <NavBar />
-      <Home />
-      <About />
+      <Home color={user.color} name={user.name} city={user.city} />
+      <About bio={user.bio} links={user.links} />
     </div>
   );
 }
